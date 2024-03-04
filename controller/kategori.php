e302d49b91351d43f60791f2ab9a1a60b33fbcf0
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-04 13:04:28 +0700

    Create kategori.php

---
+++ controller/kategori.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     *Display a listing of the resource.
+     * @return\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        echo "<h1>ini index kategori</h1>"        
+    }
+}
+
+
+?>
